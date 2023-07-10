487f7afeca762858d40e72be40ce10dd0dfe77c7
KeshaSun <innokturn@yandex.ru>
2023-07-10 19:25:51 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://6ec398f2-7fa1-43b0-a4a6-0bd3976dccb8.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/db/resources/{table_name}.csv"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
